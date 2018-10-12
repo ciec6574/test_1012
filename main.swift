ed66b0e914df6b13f3028553c6ce5f8b1b04cdc4
ciec6574 <2283199586@qq.com>
2018-10-12 20:10:30 +0800

    What

---
+++ main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  test_1012
+//
+//  Created by s20171106574 on 2018/10/12.
+//  Copyright © 2018年 s20171106574. All rights reserved.
+//
+
+import Foundation
+
+var a = 20
+var b = 20
+var c = 20
+print(a+b+c)
+print(a-b-c)
+print(a*b*c)
+print(a/b)
+
+
